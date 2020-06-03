32c186c3156ea1ff4aa6da29cb819e44576eb39f
piotr dominiak <pdominia@metal.agh.edu.pl>
2020-06-03 23:15:29 +0200

    Add project files.

---
+++ FuzzyLogicUX/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace FuzzyLogicUX
+{
+    partial class SideMenuPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.mmExit = new System.Windows.Forms.Button();
+            this.smpanelMainMenu = new System.Windows.Forms.Panel();
+            this.smReset = new System.Windows.Forms.Button();
+            this.smGo = new System.Windows.Forms.Button();
+            this.mmMainMenu = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.mwBottomPane = new System.Windows.Forms.Panel();
+            this.mwMainPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.smpanelMainMenu.SuspendLayout();
+            this.mwBottomPane.SuspendLayout();
+            this.mwMainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(7)))), ((int)(((byte)(11)))));
+            this.panel1.Controls.Add(this.mmExit);
+            this.panel1.Controls.Add(this.smpanelMainMenu);
+            this.panel1.Controls.Add(this.mmMainMenu);
+            this.panel1.Controls.Add(this.panelLogo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 361);
+            this.panel1.TabIndex = 0;
+            // 
+            // mmExit
+            // 
+            this.mmExit.Dock = System.Windows.Forms.DockStyle.Top;
+            this.mmExit.FlatAppearance.BorderSize = 0;
+            this.mmExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mmExit.Location = new System.Drawing.Point(0, 183);
+            this.mmExit.Name = "mmExit";
+            this.mmExit.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.mmExit.Size = new System.Drawing.Size(250, 45);
+            this.mmExit.TabIndex = 2;
+            this.mmExit.Text = "Exit";
+            this.mmExit.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.mmExit.UseVisualStyleBackColor = true;
+            this.mmExit.Click += new System.EventHandler(this.mmExit_Click);
+            // 
+            // smpanelMainMenu
+            // 
+            this.smpanelMainMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
+            this.smpanelMainMenu.Controls.Add(this.smReset);
+            this.smpanelMainMenu.Controls.Add(this.smGo);
+            this.smpanelMainMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.smpanelMainMenu.Location = new System.Drawing.Point(0, 97);
+            this.smpanelMainMenu.Name = "smpanelMainMenu";
+            this.smpanelMainMenu.Size = new System.Drawing.Size(250, 86);
+            this.smpanelMainMenu.TabIndex = 1;
+            // 
+            // smReset
+            // 
+            this.smReset.Dock = System.Windows.Forms.DockStyle.Top;
+            this.smReset.FlatAppearance.BorderSize = 0;
+            this.smReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.smReset.Location = new System.Drawing.Point(0, 40);
+            this.smReset.Name = "smReset";
+            this.smReset.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.smReset.Size = new System.Drawing.Size(250, 40);
+            this.smReset.TabIndex = 1;
+            this.smReset.Text = "Reset";
+            this.smReset.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.smReset.UseVisualStyleBackColor = true;
+            this.smReset.Click += new System.EventHandler(this.smReset_Click);
+            // 
+            // smGo
+            // 
+            this.smGo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.smGo.FlatAppearance.BorderSize = 0;
+            this.smGo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.smGo.Location = new System.Drawing.Point(0, 0);
+            this.smGo.Name = "smGo";
+            this.smGo.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.smGo.Size = new System.Drawing.Size(250, 40);
+            this.smGo.TabIndex = 0;
+            this.smGo.Text = "Go";
+            this.smGo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.smGo.UseVisualStyleBackColor = true;
+            this.smGo.Click += new System.EventHandler(this.smGo_Click);
+            // 
+            // mmMainMenu
+            // 
+            this.mmMainMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.mmMainMenu.FlatAppearance.BorderSize = 0;
+            this.mmMainMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mmMainMenu.Location = new System.Drawing.Point(0, 50);
+            this.mmMainMenu.Name = "mmMainMenu";
+            this.mmMainMenu.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.mmMainMenu.Size = new System.Drawing.Size(250, 47);
+            this.mmMainMenu.TabIndex = 0;
+            this.mmMainMenu.Text = "Main menu";
+            this.mmMainMenu.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.mmMainMenu.UseVisualStyleBackColor = true;
+            this.mmMainMenu.Click += new System.EventHandler(this.mmMainMenu_Click);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(250, 50);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // mwBottomPane
+            // 
+            this.mwBottomPane.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(10)))), ((int)(((byte)(30)))));
+            this.mwBottomPane.Controls.Add(this.textBox5);
+            this.mwBottomPane.Controls.Add(this.textBox4);
+            this.mwBottomPane.Controls.Add(this.label5);
+            this.mwBottomPane.Controls.Add(this.label4);
+            this.mwBottomPane.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.mwBottomPane.Location = new System.Drawing.Point(250, 183);
+            this.mwBottomPane.Name = "mwBottomPane";
+            this.mwBottomPane.Size = new System.Drawing.Size(534, 178);
+            this.mwBottomPane.TabIndex = 1;
+            // 
+            // mwMainPanel
+            // 
+            this.mwMainPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(45)))));
+            this.mwMainPanel.Controls.Add(this.textBox3);
+            this.mwMainPanel.Controls.Add(this.textBox2);
+            this.mwMainPanel.Controls.Add(this.textBox1);
+            this.mwMainPanel.Controls.Add(this.label3);
+            this.mwMainPanel.Controls.Add(this.label2);
+            this.mwMainPanel.Controls.Add(this.label1);
+            this.mwMainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mwMainPanel.Location = new System.Drawing.Point(250, 0);
+            this.mwMainPanel.Name = "mwMainPanel";
+            this.mwMainPanel.Size = new System.Drawing.Size(534, 183);
+            this.mwMainPanel.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(135, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Player Level";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ability Level";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dice Roll";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(275, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(275, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(275, 137);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(87, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 24);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Test Score";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(87, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 24);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Test Result";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(275, 31);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(275, 80);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 7;
+            // 
+            // SideMenuPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.mwMainPanel);
+            this.Controls.Add(this.mwBottomPane);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ForeColor = System.Drawing.Color.Gainsboro;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(1000, 500);
+            this.MinimumSize = new System.Drawing.Size(650, 400);
+            this.Name = "SideMenuPanel";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.smpanelMainMenu.ResumeLayout(false);
+            this.mwBottomPane.ResumeLayout(false);
+            this.mwBottomPane.PerformLayout();
+            this.mwMainPanel.ResumeLayout(false);
+            this.mwMainPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button mmExit;
+        private System.Windows.Forms.Panel smpanelMainMenu;
+        private System.Windows.Forms.Button smReset;
+        private System.Windows.Forms.Button smGo;
+        private System.Windows.Forms.Button mmMainMenu;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Panel mwBottomPane;
+        private System.Windows.Forms.Panel mwMainPanel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ FuzzyLogicUX/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FuzzyLogicUX
+{
+    public partial class SideMenuPanel : Form
+    {
+        public SideMenuPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void customizeDesign()
+        {
+
+            smpanelMainMenu.Visible = true;
+           // mmExit.Visible = true;
+        }
+
+        private void hideSubmenu()
+        {
+            if (smpanelMainMenu.Visible == true)
+                smpanelMainMenu.Visible = false;
+        }
+
+        private void showSubMenu(Panel subMenuName)
+        {
+            if (subMenuName.Visible == false)
+            {
+                hideSubmenu();
+                subMenuName.Visible = true;
+
+            }
+            else
+                subMenuName.Visible = false;  
+        }
+
+        private void mmMainMenu_Click(object sender, EventArgs e)
+        {
+            showSubMenu(smpanelMainMenu);
+        }
+
+        private void smGo_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void smReset_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mmExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
